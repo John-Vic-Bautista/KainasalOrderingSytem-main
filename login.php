cda3a1d8eb2562e47735f4e26b5996dbe44bd5c0
John-Vic-Bautista <bautistajohnvic202@gmail.com>
2023-12-04 14:08:16 +0800

    -commit

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+// Establish a connection to your MySQL database
+$servername = "your_servername";
+$username = "your_username";
+$password = "your_password";
+$database = "kainasal";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get user input from the form
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Validate user credentials (You should compare hashed passwords in a real scenario)
+$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "Login successful!";
+} else {
+    echo "Invalid username or password";
+}
+
+// Close the database connection
+$conn->close();
+?>
+
